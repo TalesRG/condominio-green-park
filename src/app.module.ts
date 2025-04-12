70611b1030ed7b2b95ee769fdbd996425dfb9f9d
TalesRG <talesrodriguesgoncalves98@gmail.com>
2025-04-12 05:26:49 -0300

    raw(README): adiciona .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { BoletoModule } from './module/boleto.module';
+import process from 'node:process';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'portaria',
+      host: String(process.env.HOST_DB),
+      port: Number(process.env.PORT_DB),
+      username: String(process.env.DB_USER),
+      password: String(process.env.PASSWORD),
+      database: String(process.env.DATABASE),
       entities: [__dirname + '/entity/**/*{.ts,.js}'],
       migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
       synchronize: false,
